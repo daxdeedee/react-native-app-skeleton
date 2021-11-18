0292af25d712e41b5c3e307e34959175092bec82
actbase <actbase@actbaseui-MacBookPro.local>
2021-11-18 20:10:53 +0900

    babel-plugin-root-import 설정 추가
    babel-plugin-root-import 설정 추가

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'babel-plugin-root-import',
+      {
+        paths: [
+          { rootPathSuffix: '.', rootPathPrefix: '/' },
+          { rootPathPrefix: './src/components', rootPathSuffix: '~/components/*' },
+          { rootPathPrefix: './src/screens', rootPathSuffix: '~/screens/*' },
+        ],
+      },
+    ],
+  ],
 };
